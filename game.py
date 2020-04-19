178c983c0f86c16e65f27c972f182151449063c5
Aleksander Jaworski <aleck156@gmail.com>
2020-04-19 10:35:13 +0200

    defined Deck class

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 # initial set up
 # setting up global variables
+import random   # required for shuffling a deck of cards
+
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
          'Queen':10, 'King':10, 'Ace':11}
+
 
 playing = True
 
@@ -33,4 +36,15 @@ class Deck():
     def __init__(self):
         self.deck = []  # start with an empty list
         for suit in suits:
-            for rank i
+            for rank in ranks:
+                pass
+
+    def __str__(self):
+        pass
+
+    def shuffle(self):
+        pass
+
+    def deal(self):
+        pass
+
